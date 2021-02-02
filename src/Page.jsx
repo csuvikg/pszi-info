869e742829dc5df9eac87009c036cc07827936f1
Gabor Csuvik <gabor.csuvik@intland.com>
2021-02-03 00:38:43 +0100

    Change drawer type

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,6 +1,8 @@
-import {Fragment, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {
     AppBar,
+    CssBaseline,
+    Divider,
     Drawer,
     IconButton,
     InputAdornment,
@@ -8,28 +10,89 @@ import {
     ListItem,
     ListItemIcon,
     ListItemText,
-    makeStyles,
     TextField,
-    Toolbar
+    Toolbar,
+    Typography
 } from "@material-ui/core";
-import {Domain as DomainIcon, Menu as MenuIcon, Search as SearchIcon} from "@material-ui/icons";
-import clsx from 'clsx';
+import {
+    ChevronLeft as ChevronLeftIcon,
+    Explore as MapIcon,
+    SupervisorAccount as AdminIcon,
+    Menu as MenuIcon,
+    Search as SearchIcon
+} from "@material-ui/icons";
+import {makeStyles} from '@material-ui/core/styles';
+import clsx from "clsx";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-    drawerPaper: {
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
         width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9) + 1,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
     },
     searchBar: {
         marginLeft: "auto"
-    },
-    toolbar: theme.mixins.toolbar
+    }
 }))
 
 const Page = ({title}) => {
-    const [open, setOpen] = useState(false);
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
     useEffect(() => {
         document.title = title
     }, [title]);
@@ -37,12 +100,29 @@ const Page = ({title}) => {
     const handleDrawerToggle = () => setOpen(!open);
 
     return (
-        <Fragment>
-            <AppBar position="absolute">
-                <Toolbar className={classes.toolbar}>
-                    <IconButton onClick={handleDrawerToggle}>
+        <div className={classes.root}>
+            <CssBaseline/>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerToggle}
+                        edge="start"
+                        className={clsx(classes.menuButton, {
+                            [classes.hide]: open,
+                        })}
+                    >
                         <MenuIcon/>
                     </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Ellátótérkép
+                    </Typography>
                     <TextField
                         className={classes.searchBar}
                         InputProps={{
@@ -55,24 +135,66 @@ const Page = ({title}) => {
                     />
                 </Toolbar>
             </AppBar>
-            <Drawer anchor="left"
-                    open={open}
-                    onClose={handleDrawerToggle}
-                    classes={{
-                        paper: classes.drawerPaper,
-                    }}
-                    ModalProps={{
-                        keepMounted: true
-                    }}
+            <Drawer
+                variant="permanent"
+                className={clsx(classes.drawer, {
+                    [classes.drawerOpen]: open,
+                    [classes.drawerClose]: !open,
+                })}
+                classes={{
+                    paper: clsx({
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    }),
+                }}
             >
+                <div className={classes.toolbar}>
+                    <IconButton onClick={handleDrawerToggle}>
+                        <ChevronLeftIcon/>
+                    </IconButton>
+                </div>
+                <Divider/>
                 <List>
                     <ListItem button>
-                        <ListItemIcon><DomainIcon/></ListItemIcon>
+                        <ListItemIcon><MapIcon/></ListItemIcon>
                         <ListItemText primary="Intézménykereső"/>
                     </ListItem>
                 </List>
+                <Divider/>
+                <List>
+                    <ListItem button>
+                        <ListItemIcon><AdminIcon/></ListItemIcon>
+                        <ListItemText primary="Admin"/>
+                    </ListItem>
+                </List>
             </Drawer>
-        </Fragment>
+            <main className={classes.content}>
+                <div className={classes.toolbar}/>
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                    donec massa sapien faucibus et molestie ac.
+                </Typography>
+                <Typography paragraph>
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+                </Typography>
+            </main>
+        </div>
     );
 };
 
