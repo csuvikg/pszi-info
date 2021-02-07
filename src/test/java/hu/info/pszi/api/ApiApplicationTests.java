1feb4238eef05ef59435031d4b20876845eca771
Gabor Csuvik <gaborcsuvik@gmail.com>
2021-02-07 22:49:10 +0100

    Init project

---
+++ src/test/java/hu/info/pszi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.info.pszi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
