1094aee9fd2ad13a0ca5f7eba818c3bde20ae0fd
csuvikg <gaborcsuvik@gmail.com>
2021-04-17 23:07:09 +0200

    Change docker-compose, routes

--- ui/src/services/routes.jsx
+++ ui/src/services/routes.jsx
@@ -1,14 +1,12 @@
-const BASE_URL = `${process.env.REACT_APP_API_SCHEMA}://${process.env.REACT_APP_API_DOMAIN}:${process.env.REACT_APP_API_PORT}`
-const ARTICLES_PATH = "articles"
-const PROVIDERS_PATH = "providers"
+const BASE_URL = "https://api.pszi.info.hu"
 
 export const routes = {
     articles: {
-        list: `${BASE_URL}/${ARTICLES_PATH}`,
-        version: `${BASE_URL}/${ARTICLES_PATH}/version`
+        list: `${BASE_URL}/articles`,
+        version: `${this.routes.articles.list}/version`
     },
     providers: {
-        list: `${BASE_URL}/${PROVIDERS_PATH}`,
-        version: `${BASE_URL}/${PROVIDERS_PATH}/version`
+        list: `${BASE_URL}/providers`,
+        version: `${this.routes.providers.list}/version`
     }
 }
