9510c19e18abf12d5b432fd7b208cf5b10c8a7f3
csuvikg <gaborcsuvik@gmail.com>
2021-05-24 03:43:13 +0200

    Fix broken test

--- ui/src/components/menu/DrawerItemList.test.js
+++ ui/src/components/menu/DrawerItemList.test.js
@@ -2,13 +2,17 @@ import {render, screen} from "@testing-library/react";
 import {DrawerItemList} from "./DrawerItemList";
 import {ITEM_TYPE} from "./DrawerItem";
 import {HashRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "../../services/store";
 
 test("renders items passed", () => {
     render(<HashRouter>
-        <DrawerItemList items={[
-            {label: "Test1", type: ITEM_TYPE.ADMIN, path: "/test/1"},
-            {label: "Test2", type: ITEM_TYPE.MAP, path: "/test/2"},
-        ]}/>
+        <Provider store={store}>
+            <DrawerItemList items={[
+                {label: "Test1", type: ITEM_TYPE.ADMIN, path: "/test/1"},
+                {label: "Test2", type: ITEM_TYPE.MAP, path: "/test/2"},
+            ]}/>
+        </Provider>
     </HashRouter>);
 
     expect(screen.getByText("Test1")).toBeInTheDocument();
